a349f2b4212288a1350eba9e3be3211cc56e29b4
unknown <m.lebon@rubika-edu.re>
2025-09-24 15:33:57 +0400

    Pause de l'après-midi, prise de décision de la création du chemin dans le maze et création d'une prefab porte

--- Assets/Scripts/Maze.cs
+++ Assets/Scripts/Maze.cs
@@ -6,40 +6,64 @@ public class Maze : MonoBehaviour
     GameObject[,] cases;
     [SerializeField]
     GameObject casePrefab;
-    
-    // Il me faut au démarrage une génération aléatoire d'un labyrinthe:
-    // Des cases avec des ouvertures orientées de façon à créer des chemins
-    // 1) Générations des cases
+    GameObject portePrefab;
+
+    // Il me faut au démarrage une génération aléatoire d'un labyrinthe et d'un trésor
+    // Labyrinthe = Des cases avec des ouvertures orientées de façon à créer des chemins
+
 
 
     void Start()
     {
-        int scale = 2;
+        // 1) Générations des cases
+        
         cases = new GameObject[100, 100];
 
         for (int n = 0; n < 100; n++)
         {
             for (int i = 0; i < 100; i++)
             {
+                int randRota = Random.Range(0, 4);
+                if (randRota == 0)
+                {
+                    cases[n, i] = Instantiate(casePrefab, new Vector3(n * 2f, 0, i*3f), new Quaternion(0f, 0f, 0f, 0f));
 
-                cases[n, i] = Instantiate(casePrefab, new Vector3(n * scale, 0, i), Quaternion.identity);
-                cases[n, i].transform.localScale = new Vector3(scale, scale, scale);
-
+                }
+                if (randRota == 1)
+                {
+                    cases[n, i] = Instantiate(casePrefab, new Vector3(n * 2f, 0, i * 3f), new Quaternion(0f, 90f, 0f, 0f));
+                }
+                if (randRota == 2)
+                {
+                    cases[n, i] = Instantiate(casePrefab, new Vector3(n * 2f, 0, i * 3f), new Quaternion(0f, -90f, 0f, 0f));
+                }
+                if (randRota == 3)
+                {
+                    cases[n, i] = Instantiate(casePrefab, new Vector3(n *2f, 0, i * 3f), new Quaternion(0f, 180f, 0f, 0f));
+                }
+                cases[n, i].transform.localScale = new Vector3(2f, 7, 3f);
+                
+                
             }
 
         }
+
 
         StartCoroutine(Clignotage());
 
 
     }
 
-    
+    int indexDeLaCaseCourante = 0;
+
 
     // Update is called once per frame
     void Update()
     {
-
+        if (indexDeLaCaseCourante >= 1000) return;
+        cases[indexDeLaCaseCourante, 0] = Instantiate(portePrefab, new Vector3 ())
+        cases[indexDeLaCaseCourante + 1, 0].GetComponent<Case>().OuvrirOuest();
+        indexDeLaCaseCourante++;
 
     }
 
