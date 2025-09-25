4b0953172e7e7564f112a3b6a661f8dbe9a914db
unknown <m.lebon@rubika-edu.re>
2025-09-25 16:54:28 +0400

    Abandon des portes, j'ai opté pour l'ouvertures de 2 mur de chaque case, script non fonctionnel pour l'heure, Ajout : update chaque secondes et non chaque frame

--- Assets/Scripts/Maze.cs
+++ Assets/Scripts/Maze.cs
@@ -10,12 +10,7 @@ public class Maze : MonoBehaviour
     GameObject casePrefab;
     [SerializeField]
     GameObject cagePrefab;
-    [SerializeField]
-    GameObject portePrefab;
-    [SerializeField]
-    GameObject joueur;
 
-    
     GameObject cage;
 
 
@@ -36,13 +31,13 @@ public class Maze : MonoBehaviour
         // 1) Générations des cases  aléatoirement
         cases = new GameObject[100, 100];
 
-        for (int n = 0; n < 100; n++)
+        for (int i = 0; i <= 100; i++)
         {
-            for (int i = 0; i < 100; i++)
+            for (int j = 0; j <= 100; j++)
             {
 
 
-                cases[n, i] = Instantiate(casePrefab, new Vector3(n *5, 0, i*5 ), Quaternion.identity);
+                cases[i, j] = Instantiate(casePrefab, new Vector3(i *5, 0, j*5 ), Quaternion.identity);
                 
             }
         }
@@ -50,96 +45,51 @@ public class Maze : MonoBehaviour
         StartCoroutine(Libère());
     }
 
-    int indexLigneCourante = 0;
-    int indexColonneCourante = 0;
 
     //Permet de lancer la réapparition des murs après un certain temps donné
-    IEnumerator OuvreToiSesame(int ligneCourante, int colonneCourante)
+    IEnumerator FermeToiSesame()
     {
         yield return new WaitForSeconds(3f);
-        cases[ligneCourante, colonneCourante].GetComponent<Chemin>().FermerTousLesMurs();
-        cases[ligneCourante, colonneCourante] = Instantiate(casePrefab, new Vector3(cases[ligneCourante, colonneCourante].transform.position.x, 0, cases[ligneCourante, colonneCourante].transform.position.z), Quaternion.identity);
+
+        for (int i = 0; i < 100; i++)
+        {
+            for (int j = 0; j < 100; j++)
+            {
+
+
+                cases[i, j].GetComponent<Chemin>().FermerTousLesMurs();
+
+            }
+        }
+        
+       
     }
+
+    float tempsDerniereExecution = 0.0f;
+    float delai = 1f;
 
 
     // Update is called once per frame
     void Update()
     {
-        // Je veux qu'à l'appuie sur O, 2 portes apparaissent sur 2 des murs de la case où se trouve le joueur puis se referme après un instant
+        // Je veux qu'à chaque Frame  2 des murs de chaque case s'ouvrent puis se referme après un instant
 
-        int ligneCourante = indexLigneCourante;
-        int colonneCourante = indexLigneCourante;
-
-
-        if (Input.GetKeyDown(KeyCode.O))
+       
         {
-            // 1) Il faut ouvrir les 2 Murs aléatoirement selon la case où est le joueur
-            for (int i = 0; i < 2; i++)
+            // 1) Il faut ouvrir les 2 Murs aléatoirement sur chaque case
+
+
+            tempsDerniereExecution += Time.fixedDeltaTime;  // ajoute a chaque update le temps écoulé depuis le dernier Update		
+            if (tempsDerniereExecution > delai)
             {
-                int randWall = Random.Range(0, 4);
-                if (ligneCourante == 0)
-                {
-                    randWall = Random.Range(0, 3);
-                }
-                if (colonneCourante == 0)
-                {
-                    randWall = Random.Range(1, 4);
-                }
-                if (ligneCourante == 0 && colonneCourante == 0)
-                {
-                    randWall = Random.Range(1, 3);
-                }
-                if (cases[ligneCourante, colonneCourante] == portePrefab)
-                {
-                    if (ligneCourante == 0)
-                    {
-                        randWall = Random.Range(0, 2);
-                    }
-                    if (colonneCourante == 0)
-                    {
-                        randWall = Random.Range(1, 3);
-                    }
-                    if (ligneCourante == 0 && colonneCourante == 0)
-                    {
-                        randWall = Random.Range(1, 2);
-                    }
-                }
-                if (randWall == 0)
-                {
-
-                    cases[ligneCourante, colonneCourante].GetComponent<Chemin>().OuvrirSud();
-                    // 2) Remplacer les Murs par des portes
-                    cases[ligneCourante, colonneCourante] = Instantiate(portePrefab, new Vector3(cases[ligneCourante, colonneCourante].GetComponent<Walls>().transform.position.x, 0, cases[ligneCourante, colonneCourante].GetComponent<Walls>().transform.position.z), Quaternion.identity);
-                    cases[colonneCourante, colonneCourante - 1].GetComponent<Chemin>().OuvrirNord();
-
-                }
-
-
-                if (randWall == 1)
-                {
-
-                    cases[ligneCourante, colonneCourante].GetComponent<Chemin>().OuvrirEst();
-                    cases[ligneCourante, colonneCourante] = Instantiate(portePrefab, new Vector3(cases[ligneCourante, colonneCourante].transform.position.x*3, 0, cases[ligneCourante, colonneCourante].transform.position.z*3), Quaternion.identity);
-                    cases[ligneCourante + 1, colonneCourante].GetComponent<Chemin>().OuvrirOuest();
-                }
-
-                if (randWall == 2)
-                {
-                    cases[ligneCourante, colonneCourante].GetComponent<Chemin>().OuvrirNord();
-                    cases[ligneCourante, colonneCourante] = Instantiate(portePrefab, new Vector3(cases[ligneCourante, colonneCourante].transform.position.x*3, 0, cases[ligneCourante, colonneCourante].transform.position.z*3), Quaternion.identity);
-                    cases[ligneCourante, colonneCourante + 1].GetComponent<Chemin>().OuvrirSud();
-                }
-
-                if (randWall == 3)
-                {
-
-                    cases[ligneCourante, ligneCourante].GetComponent<Chemin>().OuvrirOuest();
-                    cases[ligneCourante, colonneCourante] = Instantiate(portePrefab, new Vector3(cases[ligneCourante, colonneCourante].transform.position.x*3, 0, cases[ligneCourante, colonneCourante].transform.position.z*3), Quaternion.identity);
-                    cases[ligneCourante - 1, colonneCourante].GetComponent<Chemin>().OuvrirEst();
-                }
+                OpenPathRandom();
+                tempsDerniereExecution = 0;
+                StartCoroutine(FermeToiSesame());
             }
-            // 3) Attendre un temps puis faire la manoeuvre inverse
-            StartCoroutine(OuvreToiSesame(ligneCourante, ligneCourante));
+
+
+            // 2) Attendre un temps puis faire la manoeuvre inverse
+            
 
             
 
@@ -148,6 +98,55 @@ public class Maze : MonoBehaviour
         
 
     }
+
+    
+    
+
+     private void OpenPathRandom()
+    {
+        for (int n = 0; n <= 1; n++)
+        {
+
+            for (int i = 0; i <= 100; i++)
+            {
+
+                for (int j = 0; j <= 100; j++)
+                {
+                    int randWall = Random.Range(0, 4);
+                    if (randWall == 0)
+                    {
+
+                        cases[i, j].GetComponent<Chemin>().OuvrirSud();
+                        cases[i, j].GetComponent<Chemin>().OuvrirNord();
+                    }
+
+
+
+                    if (randWall == 1)
+                    {
+
+                        cases[i, j].GetComponent<Chemin>().OuvrirEst();
+                        cases[i, j].GetComponent<Chemin>().OuvrirOuest();
+                    }
+
+                    if (randWall == 2)
+                    {
+                        cases[i, j].GetComponent<Chemin>().OuvrirNord();
+                        cases[i, j].GetComponent<Chemin>().OuvrirSud();
+                    }
+
+                    if (randWall == 3)
+                    {
+
+                        cases[i, j].GetComponent<Chemin>().OuvrirOuest();
+                        cases[i, j].GetComponent<Chemin>().OuvrirEst();
+                    }
+                }
+            }
+        }
+    }   
+
+    
 
 
 
@@ -165,9 +164,9 @@ public class Maze : MonoBehaviour
         {
             // 1) Désactiver les murs un instant 
             yield return new WaitForSeconds(0.45f);
-            for (int i = 0; i < 100; i++)
+            for (int i = 0; i <= 100; i++)
             {
-                for (int j = 0; j < 100; j++)
+                for (int j = 0; j <= 100; j++)
                 {
                     cases[i, j].GetComponent<Chemin>().OuvrirTousLesMurs();
                 }
